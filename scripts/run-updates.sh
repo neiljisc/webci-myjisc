48d8aff675cdb9b1e308458af4355eb0b8c7e524
Neil McKett <neil.mckett@jisc.ac.uk>
2020-03-26 00:02:21 +0000

    Fix update script so that it defaults to the 'updates' directory.

--- scripts/run-updates.sh
+++ scripts/run-updates.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 
 OLDPWD=$PWD
+directory=$1
+if [ "X$directory" = "X" ] ; then
+    directory="updates"
+fi
 
 cd `dirname $0`
 
-for file in `php parseGitLog.php $OLDPWD/$1`
+for file in `php parseGitLog.php $OLDPWD/$directory`
 do
 testUpdate=`./checkIfUpdateApplied $file`
 if [ "X$testUpdate" = "X" ] ; then
